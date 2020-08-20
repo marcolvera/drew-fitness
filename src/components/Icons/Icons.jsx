fab11ad5cc9f6da8b7cc18965785304fd9b6cb00
marco <marcolvera18@yahoo.com>
2020-08-19 18:49:45 -0700

    create icons comp

---
+++ src/components/Icons/Icons.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Icons.module.css';
+
+const Icons = () => {
+    return (
+        <div className={styles.box}>
+            <div className={styles.subBox}>
+                <img className={styles.img} src="https://i.imgur.com/DKgdzsn.png" alt=""/>
+                <h3 className={styles.h3}>Investment Banking</h3>
+                <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</p>
+            </div>
+            <div className={styles.subBox}>
+                <img className={styles.img} src="https://i.imgur.com/OeGnZ4i.png" alt=""/>
+                <h3 className={styles.h3}>Portfolio Management</h3>
+                <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</p>
+            </div>
+            <div className={styles.subBox}>
+                <img className={styles.img} src="https://i.imgur.com/Mdi2oQJ.png" alt=""/>
+                <h3 className={styles.h3}>Tax &amp; Custodial</h3>
+                <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, sapiente!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Icons;
--- src/components/MainFill/MainFill.jsx
+++ src/components/MainFill/MainFill.jsx
@@ -6,14 +6,14 @@ const MainFill = () => {
     return (
         <div className={styles.box}>
            <div className={styles.txtBox}>
-               <h1 className={styles.h1}>ANDREW</h1>
+               <h1 className={styles.h1}>AN<span>DREW</span></h1>
                <h1 className={styles.h1}>PERSONAL</h1>
                <h1 className={styles.h1}>FITNESS</h1>
                <h1 className={styles.h1}>TRAINER</h1>
                <Link className={styles.btn}>BOOK NOW</Link>
            </div>
            <div>
-               <img src="/back.jpg" alt=""/>
+               <img className={styles} src="/back.jpg" alt=""/>
            </div>
         </div>
     )
