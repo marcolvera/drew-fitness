eeb8db95eb45ee17479307d4d2304fe69eb57197
marco <marcolvera18@yahoo.com>
2020-08-20 12:47:19 -0700

    create card2 comp

---
+++ src/components/Card2/Card2.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Blog.module.css';
+import { Link } from 'react-router-dom';
+
+const Blog = () => {
+    return (
+        <div className={styles.row}>
+            <div className={styles.column}>
+                <div className={styles.column2}>
+                    <h4 className={styles.h4}>July 4 2020</h4>
+                    <h2 className={styles.h2}>Blog Post One</h2>
+                    <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perspiciatis 
+                    ea ullam quidem officiis, assumenda sunt ducimus, laudantiume quaerat deserunt soluta placeat dolorum, quis mollitia 
+                    voluptates maiores saepe possimus velit quibusdam. Delectus perspiciatis expedita aliquid.</p>
+                    <Link className={styles.btn}>READ OUR BLOG</Link>
+                </div>
+            </div>
+            <div className={styles.column}>
+                <div className={styles.column1}>
+                    <img className={styles.img} src="https://i.imgur.com/MymeI1w.jpg" alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Blog
