97cda515c40dac730cc341cf758abc8b0dd30f6f
marco <marcolvera18@yahoo.com>
2020-08-23 19:40:50 -0700

    update first row imgs

--- src/components/Card3/Card3.jsx
+++ src/components/Card3/Card3.jsx
@@ -5,18 +5,18 @@ import styles from './Card3.module.css';
 
 const Card3 = () => {
     return(
-        <div>
+        <div className={styles.container}>
             <div className={styles.box}>
-                <h4 className={styles.h4}>This is what we do</h4>
-                <h2 className={styles.h2}>Business Cases</h2>
+                <h4 className={styles.h4}>Choose what is right for you</h4>
+                <h2 className={styles.h2}>Types Of Programs</h2>
                 <p className={styles.p}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas facere 
                 similique praesentium</p>
             </div>
             <div className>
                 <div className={styles.row}>
-                    <img className={styles.img} src="https://i.imgur.com/P5YQZUC.jpg" alt=""/>
-                    <img className={styles.img} src="https://i.imgur.com/3fCnoVX.jpg" alt=""/>
-                    <img className={styles.img} src="https://i.imgur.com/lm6RPHu.jpg" alt=""/>
+                    <img className={styles.img} src="pic1.jpg" alt=""/>
+                    <img className={styles.img} src="pic2.jpg" alt=""/>
+                    <img className={styles.img} src="pic3.jpg" alt=""/>
                 </div>
                 <div className={styles.row}>
                     <img className={styles.img} src="https://i.imgur.com/NdzRZB6.jpg" alt=""/>
