2df77518b662ce81a58b9bb8e3870c0490e4a375
marco <marcolvera18@yahoo.com>
2020-08-23 19:29:53 -0700

    update card3 images and flex

--- src/components/Card3/Card3.jsx
+++ src/components/Card3/Card3.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import styles from './Card3.module.css';
 
+
+
 const Card3 = () => {
     return(
         <div>
-            <div className={styles.toptxt}><h1 className={styles.h1S}>LETS GET TO WORK</h1></div>
             <div className={styles.box}>
-                <div>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                    </ul>
+                <h4 className={styles.h4}>This is what we do</h4>
+                <h2 className={styles.h2}>Business Cases</h2>
+                <p className={styles.p}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas facere 
+                similique praesentium</p>
+            </div>
+            <div className>
+                <div className={styles.row}>
+                    <img className={styles.img} src="https://i.imgur.com/P5YQZUC.jpg" alt=""/>
+                    <img className={styles.img} src="https://i.imgur.com/3fCnoVX.jpg" alt=""/>
+                    <img className={styles.img} src="https://i.imgur.com/lm6RPHu.jpg" alt=""/>
                 </div>
-                <div></div>
+                <div className={styles.row}>
+                    <img className={styles.img} src="https://i.imgur.com/NdzRZB6.jpg" alt=""/>
+                    <img className={styles.img} src="https://i.imgur.com/I7x4Ezx.jpg" alt=""/>
+                    <img className={styles.img} src="https://i.imgur.com/2XiK6CN.jpg" alt=""/>
+                </div>
             </div>
         </div>
     )
-};
+}
 
 export default Card3;
                      
