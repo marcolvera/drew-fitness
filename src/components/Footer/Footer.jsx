93f5e250b31200943dcf8b64a2be2cd2e8dfea26
marco <marcolvera18@yahoo.com>
2020-08-26 13:53:22 -0700

    create footer comp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from './Footer.module.css'
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const Footer = () => {
     return(
-        <div>Hello World</div>
+        <div className={styles.container}>
+            <div className={styles.box}>
+                <InstagramIcon style ={{ fontSize: 40 }}/>
+                <TwitterIcon style ={{ fontSize: 40 }}/>
+                <FacebookIcon style ={{ fontSize: 40 }}/>
+            </div>
+            <p className={styles.p}>Copyright&copy;2020 DrewFitness</p>
+        </div>
     );
 }
 
