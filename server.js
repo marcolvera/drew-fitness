65b1eee173e2c74819e3752ebb36b9fdd8134734
marco <marcolvera18@yahoo.com>
2020-08-26 14:13:45 -0700

    run build

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
  // load secrets from .env file
  require('dotenv').config();
